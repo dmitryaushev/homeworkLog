7765a2f08940567186b9c9f976b66011a9332c8a
DAushev <DAushev@DAushev-NB.luxoft.com>
2021-02-12 15:13:13 +0200

    Added actions to MenuBar

--- src/main/java/com/luxoft/homeworkLog/ui/action/Delete.java
+++ src/main/java/com/luxoft/homeworkLog/ui/action/Delete.java
@@ -1,12 +1,24 @@
 package com.luxoft.homeworkLog.ui.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.window.ApplicationWindow;
+
+import com.luxoft.homeworkLog.model.ModelManager;
+import com.luxoft.homeworkLog.model.Student;
+import com.luxoft.homeworkLog.ui.ViewManager;
 
 public class Delete extends Action {
 
-	ApplicationWindow _applicationWindow;
+	private ApplicationWindow _applicationWindow;
+
+	private ModelManager _modelManager = ModelManager.getInstance();
+	private ViewManager _viewManager = ViewManager.getInstance();
 
 	public Delete(ApplicationWindow applicationWindow) {
 		_applicationWindow = applicationWindow;
@@ -15,9 +27,34 @@ public class Delete extends Action {
 
 	@Override
 	public void run() {
-		boolean result = MessageDialog.openQuestion(_applicationWindow.getShell(), "Delete", "Delete data?");
+		
+		TableViewer tableViewer = _viewManager.getStudentListUI().getTableViewer();
+		List<Student> students = new ArrayList<>();
+		
+		StructuredSelection structuredSelection = (StructuredSelection) tableViewer.getSelection();
+		structuredSelection.forEach(selectedStudent -> {
+			Student student = (Student) selectedStudent;
+			students.add(student);
+		});
+		
+		String message;
+		if (students.size() == 0) {
+			MessageDialog.openError(_applicationWindow.getShell(), "Invalid", "No student select");
+			return;
+		}
+		if (students.size() == 1) {
+			message = String.format("Delete student %s?", students.get(0).getName());
+		} else {
+			StringBuilder studentsName = new StringBuilder();
+			students.forEach(student -> studentsName.append(student.getName()).append(", "));
+			studentsName.delete(studentsName.length() - 2, studentsName.length());
+			message = String.format("Delete students: %s?", studentsName.toString());
+		}
+		
+		boolean result = MessageDialog.openQuestion(_applicationWindow.getShell(), "Delete", message);
 		if (result) {
-
+			_modelManager.deleteStudents(students);
+			tableViewer.refresh();
 		}
 	}
 }
--- src/main/java/com/luxoft/homeworkLog/ui/action/Save.java
+++ src/main/java/com/luxoft/homeworkLog/ui/action/Save.java
@@ -1,12 +1,24 @@
 package com.luxoft.homeworkLog.ui.action;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.ApplicationWindow;
 
+import com.google.gson.Gson;
+import com.luxoft.homeworkLog.model.ModelManager;
+import com.luxoft.homeworkLog.model.Student;
+
 public class Save extends Action {
 
-	ApplicationWindow _applicationWindow;
+	private ApplicationWindow _applicationWindow;
+	
+	private ModelManager _modelManager = ModelManager.getInstance();
 
 	public Save(ApplicationWindow applicationWindow) {
 		_applicationWindow = applicationWindow;
@@ -17,7 +29,14 @@ public class Save extends Action {
 	public void run() {
 		boolean result = MessageDialog.openQuestion(_applicationWindow.getShell(), "Save", "Save data?");
 		if (result) {
-
+			List<Student> students = _modelManager.getStateModel().getStudents();
+			String json = new Gson().toJson(students);
+			String path = String.format("%s%sfile.txt", System.getProperty("user.dir"), File.separator);
+			try {
+				Files.write(Paths.get(path), json.getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
